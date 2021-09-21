88add99fd1f561a00e19a1a512a2aa0f53785e6d
Daniel Posada Calle <posadacalled@gmail.com>
2021-09-20 22:13:25 -0500

    Get procedures

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/screens/login_screen.dart';
+import 'package:vehicles_app/screens/procedures_screen.dart';
 
 class HomeScreen extends StatefulWidget { 
   final Token token;
@@ -19,21 +20,40 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Vehicles'),
       ),
       body: _getBody(),
-      drawer: _getMechanicMenu(),
+      drawer: widget.token.user.userType == 0 
+        ? _getMechanicMenu() 
+        : _getCustomerMenu(),
     );
   }
 
   Widget _getBody() {
     return Container(
       margin: EdgeInsets.all(30),
-      child: Center(
-        child: Text(
-          'Bienvenid@ ${widget.token.user.fullName}',
-          style: TextStyle(
-            fontWeight: FontWeight.bold, 
-            fontSize: 20,
-          ),
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(150),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/noimage.png'), 
+                  image: NetworkImage(widget.token.user.imageFullPath),
+                  height: 300,
+                  fit: BoxFit.cover
+              ),
+            ),      
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+              'Bienvenid@ ${widget.token.user.fullName}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold, 
+                fontSize: 20,
+              ),
+            ),
+            )
+        ],
       ),
     );
   }
@@ -56,7 +76,14 @@ class _HomeScreenState extends State<HomeScreen> {
           ListTile(
             leading: Icon(Icons.precision_manufacturing),
             title: const Text('Procedimientos'),
-            onTap: (){},
+            onTap: (){
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => ProceduresScreen(token: widget.token)
+                )
+              );
+            },
           ),
           ListTile(
             leading: Icon(Icons.badge),
@@ -73,6 +100,47 @@ class _HomeScreenState extends State<HomeScreen> {
             title: const Text('Usuarios'),
             onTap: (){},
           ),
+          Divider(
+            color: Colors.black,
+            height: 2,
+          ),
+          ListTile(
+            leading: Icon(Icons.face),
+            title: const Text('Editar Perfil'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: const Text('Cerrar Sessión'),
+            onTap: (){
+              Navigator.pushReplacement(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => LoginScreen()
+                )
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getCustomerMenu() {
+     return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Image(
+              image: AssetImage('assets/vehicles_logo.png')
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.two_wheeler),
+            title: const Text('Mis Vehiculos'),
+            onTap: (){},
+          ),        
           Divider(
             color: Colors.black,
             height: 2,
