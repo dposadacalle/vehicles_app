0bc99e7e63c092126e1392976fd90bfeb52cf95f
Daniel Posada Calle <posadacalled@gmail.com>
2021-09-21 23:20:58 -0500

    Method crud

--- lib/screens/procedures_screen.dart
+++ lib/screens/procedures_screen.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 
 import 'package:vehicles_app/components/loader_component.dart';
-import 'package:vehicles_app/helpers/constans.dart';
+import 'package:vehicles_app/helpers/api_helper.dart';
 import 'package:vehicles_app/models/procedure.dart';
+import 'package:vehicles_app/models/response.dart';
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/screens/procedure_screen.dart';
 
@@ -62,32 +63,29 @@ class _ProceduresScreenState extends State<ProceduresScreen> {
       _showLoader = true;
     });
 
-    var url = Uri.parse('${Constans.apiUrl}/api/Procedures');
-    var response = await http.get(
-      url, 
-      headers: {
-        'content-type' : 'application/json',
-        'accept' : 'application/json',
-        'authorization': 'bearer ${widget.token.token}'
-      }
-    );
+    Response response = await ApiHelper.getProcedures(widget.token.token);
 
     setState(() {
       _showLoader = false;
     });
 
-    var body = response.body;
-    var decodedJson = jsonDecode(body);
-    if(decodedJson != null){
-      for(var procedure in decodedJson){
-        _procedures.add(Procedure.fromJson(procedure));
-      }   
-
+    if(!response.isSuccess){
+      await showAlertDialog(
+        context: context,
+        title: 'Error',
+        message: response.message,
+        actions: <AlertDialogAction>[
+          AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );
+      return;
     }
 
-    print(_procedures);
+    setState(() {
+      _procedures = response.result;
+    });
 
-  }
+  } 
 
   Widget _getContent() {
     return _procedures.length == 0
